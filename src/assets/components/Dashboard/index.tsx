4b62811c756968d57c5e312d123b353477665df0
Thais Cristina <thais78863@gmail.com>
2026-04-23 23:36:18 -0400

    Delete src/assets/components/Dashboard directory

--- src/assets/components/Dashboard/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as RPGDashboard } from "./rpgdashboard.tsx";
- 
-export { RadarChart, WeightChart, ImcGauge } from "./charts";
- 
-export { COLORS, FONTS, GOOGLE_FONTS_URL } from "./foundation";
-export type { Mission, WeightEntry, RadarAttr } from ".src/assets/components/Dashboard/Base.ts"";
-
