51a624f4754860122bc8f4b9b210812ca15ce229
Thais Cristina <thais78863@gmail.com>
2026-04-23 23:38:38 -0400

    dashboard

---
+++ src/assets/components/global/pages/rpgdashboard.tsx
@@ -0,0 +1,7 @@
+export { default as RPGDashboard } from "./rpgdashboard.tsx";
+ 
+export { RadarChart, WeightChart, ImcGauge } from "./charts";
+ 
+export { COLORS, FONTS, GOOGLE_FONTS_URL } from "./foundation";
+export type { Mission, WeightEntry, RadarAttr } from ".src/assets/components/Dashboard/Base.ts"";
+
